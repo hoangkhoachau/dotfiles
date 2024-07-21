c6ad6b8a55b186dae88be8418665f853cc531b02
hoangkhoachau <21120481@student.hcmus.edu.vn>
2024-07-21 22:19:46 +0700

    use newer function for keymap

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -1,39 +1,45 @@
 -- Key Mappings
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap('n', '<Leader>r', ':10split <CR>:term ./%:r<CR>i<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F4>', ':SymbolsOutline<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>o', ':Oil<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F2>', ':25vs %:r.in<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>cp', ':w<CR> :!clang++ -std=c++20 -Wall % -o %:r<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>FzfLua files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-t>', '<cmd>FzfLua files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>FzfLua live_grep_native<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>FzfLua oldfiles<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>FzfLua buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fc', '<cmd>FzfLua lgrep_curbuf<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>FzfLua lsp_document_symbols<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fd', '<cmd>FzfLua diagnostics_document<CR>', { noremap = true, silent = true })
 
---LSP
---lsp keybindings
+vim.keymap.set('n', '<Leader>r', ':10split <CR>:term ./%:r<CR>i<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<F4>', ':SymbolsOutline<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>o', ':Oil<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<F2>', ':25vs %:r.in<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>cp', ':w<CR> :!clang++ -std=c++20 -Wall % -o %:r<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-t>', '<cmd>FzfLua files<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep_native<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fh', '<cmd>FzfLua oldfiles<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fb', '<cmd>FzfLua buffers<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fc', '<cmd>FzfLua lgrep_curbuf<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fs', '<cmd>FzfLua lsp_document_symbols<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fd', '<cmd>FzfLua diagnostics_document<CR>', { noremap = true, silent = true })
+
+vim.keymap.set({ "n", "v" }, "<leader>=", function()
+  local conform = require("conform")
+  conform.format({
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  })
+end, { desc = "Format file or range (in visual mode)" })
+
+-- LSP Keybindings
 local lsp_keybindings = function()
-    vim.api.nvim_set_keymap('n', '[d', [[:lua vim.diagnostic.goto_prev()<cr>]], {})
-    vim.api.nvim_set_keymap('n', ']d', [[:lua vim.diagnostic.goto_next()<cr>]], {})
-    vim.api.nvim_set_keymap('n','<Leader>gD',[[:lua vim.lsp.buf.declaration()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>gd',[[:lua vim.lsp.buf.definition()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>K',[[:lua vim.lsp.buf.hover()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>gr',[[:lua vim.lsp.buf.references()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>gs',[[:lua vim.lsp.buf.signature_help()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>gi',[[:lua vim.lsp.buf.implementation()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>gt',[[:lua vim.lsp.buf.type_definition()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>gw',[[:lua vim.lsp.buf.document_symbol()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>gW',[[:lua vim.lsp.buf.workspace_symbol()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>ca',[[:lua vim.lsp.buf.code_action()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>e',[[:lua vim.diagnostic.open_float()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>rn',[[:lua vim.lsp.buf.rename()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>=',[[:lua vim.lsp.buf.format()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>ic',[[:lua vim.lsp.buf.incoming_calls()<cr>]],{})
-    vim.api.nvim_set_keymap('n','<Leader>oc',[[:lua vim.lsp.buf.outgoing_calls()<cr>]],{})
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {})
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {})
+  vim.keymap.set('n', '<Leader>gD', vim.lsp.buf.declaration, {})
+  vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, {})
+  vim.keymap.set('n', '<Leader>K', vim.lsp.buf.hover, {})
+  vim.keymap.set('n', '<Leader>gr', vim.lsp.buf.references, {})
+  vim.keymap.set('n', '<Leader>gs', vim.lsp.buf.signature_help, {})
+  vim.keymap.set('n', '<Leader>gi', vim.lsp.buf.implementation, {})
+  vim.keymap.set('n', '<Leader>gt', vim.lsp.buf.type_definition, {})
+  vim.keymap.set('n', '<Leader>gw', vim.lsp.buf.document_symbol, {})
+  vim.keymap.set('n', '<Leader>gW', vim.lsp.buf.workspace_symbol, {})
+  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, {})
+  vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, {})
+  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, {})
+  vim.keymap.set('n', '<Leader>ic', vim.lsp.buf.incoming_calls, {})
+  vim.keymap.set('n', '<Leader>oc', vim.lsp.buf.outgoing_calls, {})
 end
-
-
