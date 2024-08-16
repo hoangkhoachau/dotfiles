b63f75b073f1a93c4ea4e0179ae7d50f92e310a5
hoangkhoachau <21120481@student.hcmus.edu.vn>
2024-08-16 11:38:21 +0700

    change keybindings

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<Leader>r', ':10split <CR>:term ./%:r<CR>i<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<F4>', ':SymbolsOutline<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<F4>', '<cmd>Outline<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>o', ':Oil<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>u', ":lua require('undotree').toggle()<cr>", { noremap = true, silent = true })
 vim.keymap.set('n', '<F2>', ':25vs %:r.in<CR>', { noremap = true, silent = true })
@@ -26,23 +26,22 @@ vim.keymap.set({ "n", "v" }, "<leader>=", function()
 end, { desc = "Format file or range (in visual mode)" })
 
 -- LSP Keybindings
+local toggleInlay = function()
+                local current_value = vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
+                vim.lsp.inlay_hint.enable(not current_value, { bufnr = 0 })
+            end
 lsp_keybindings = function()
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {})
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {})
-  vim.keymap.set('n', '<Leader>gD', vim.lsp.buf.declaration, {})
-  vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', '<Leader>K', vim.lsp.buf.hover, {})
-  vim.keymap.set('n', '<Leader>gr', vim.lsp.buf.references, {})
-  vim.keymap.set('n', '<Leader>gs', vim.lsp.buf.signature_help, {})
-  vim.keymap.set('n', '<Leader>gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', '<Leader>gt', vim.lsp.buf.type_definition, {})
-  vim.keymap.set('n', '<Leader>gw', vim.lsp.buf.document_symbol, {})
-  vim.keymap.set('n', '<Leader>gW', vim.lsp.buf.workspace_symbol, {})
-  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, {})
-  vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, {})
-  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', '<Leader>ic', vim.lsp.buf.incoming_calls, {})
-  vim.keymap.set('n', '<Leader>oc', vim.lsp.buf.outgoing_calls, {})
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {desc = "Previous Diagnostic"})
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {desc = "Next Diagnostic"})
+  vim.keymap.set('n', '<Leader>gD', vim.lsp.buf.declaration, {desc = "Go to declaration"})
+  vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, {desc = "Go to definition"})
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {desc = "Show Info"})
+  vim.keymap.set('n', '<Leader>gr', vim.lsp.buf.references, {desc = "Show references"})
+  vim.keymap.set('n', '<Leader>gi', vim.lsp.buf.implementation, {desc = "Go to implementation"})
+  vim.keymap.set('n', '<Leader>a', vim.lsp.buf.code_action, {desc = "Show Code Actions"})
+  vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, {desc = "Show Diagnostic"})
+  vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, { desc = "Rename" })
+  vim.keymap.set('n', '<Leader>h', toggleInlay, { desc = "Toggle Inlay hint" })
 end
 
 --xcode build
